3c424c52d1bd694120146ca1607686f58d205104
Rahul Singh <rahulsingh14jan95@gmail.com>
2024-04-03 00:52:41 +0530

    Added full support and code

---
+++ main/include/certs.h
@@ -0,0 +1,10 @@
+
+static const char *client_cert_pem =
+    "-----BEGIN CERTIFICATE-----\n"
+    ""
+    "-----END CERTIFICATE-----";
+
+static const char *private_key_pem =
+    "-----BEGIN PRIVATE KEY-----\n"
+    ""
+    "-----END PRIVATE KEY-----";
